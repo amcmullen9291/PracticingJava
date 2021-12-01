6b8c02c7ef27177a1e24baa9160bcfe407ba9c7a
Anthony <amcmullen9291@gmail.com>
2021-12-01 07:57:53 -0500

    method to get variable type

--- src/Workspace/PracticeSessions2.java
+++ src/Workspace/PracticeSessions2.java
@@ -1,4 +1,22 @@
 package Workspace;
 
-public class PracticeSessions2 {
+public class PracticeSessions2  {
+
+
+    public static void main(String[] args) {
+        getTypeOfVariable("Good Morning");
+        getTypeOfVariable(12_553);
+        getTypeOfVariable(5_236_776_900L);
+        getTypeOfVariable(1.00F);
+        getTypeOfVariable(5_3333333333334D); //double
+        getTypeOfVariable("anthony"=="Anthony");
+        getTypeOfVariable('b');
+
+    }
+
+    public static void getTypeOfVariable(Object input){
+        System.out.println(((Object) input).getClass().getSimpleName()); //readable version
+    }
+
+
 }

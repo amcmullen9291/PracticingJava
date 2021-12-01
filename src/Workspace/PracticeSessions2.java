cc714040bcff76cb22b1acfbf676495f6878b9a1
Anthony <amcmullen9291@gmail.com>
2021-12-01 08:26:28 -0500

    safety commit

--- src/Workspace/PracticeSessions2.java
+++ src/Workspace/PracticeSessions2.java
@@ -13,15 +13,19 @@ public class PracticeSessions2  {
         getTypeOfVariable("Good Morning");
         getTypeOfVariable(12_553);
         getTypeOfVariable(5_236_776_900L);
-        getTypeOfVariable(1.00F);
-        getTypeOfVariable(5_3333333333334D); //double
+        getTypeOfVariable(1F);
+        getTypeOfVariable(1.0001D);
+        getTypeOfVariable(5_334F);
+        getTypeOfVariable(5_444);
         getTypeOfVariable("anthony"=="Anthony");
         getTypeOfVariable('b');
         //user can't MAKE/use an array, hashMap, etc. without the programmer knowing it
     }
 
     public static void getTypeOfVariable(Object input){
-        System.out.println(((Object) input).getClass().getSimpleName()); //readable version
+        System.out.print(((Object) input).getClass().getSimpleName()); //readable version
+        System.out.println(": " + input);
+
     }
 
 

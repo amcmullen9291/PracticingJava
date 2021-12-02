d54e54a4b3b20e0311c7f4fdafce07fde6d421e5
Anthony <amcmullen9291@gmail.com>
2021-12-02 03:41:44 -0500

    easier version countArrayDuplicates

--- src/Workspace/PracticeSessions2.java
+++ src/Workspace/PracticeSessions2.java
@@ -1,8 +1,6 @@
 package Workspace;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeSessions2  {
 
@@ -18,7 +16,7 @@ public class PracticeSessions2  {
         getTypeOfVariable('b');
         //user can't MAKE/use an array, hashMap, etc. without the programmer knowing it
 
-//        countArrayDuplicates();
+        countArrayDuplicates();
     }
 
     public static void getTypeOfVariable(Object input){
@@ -29,26 +27,23 @@ public class PracticeSessions2  {
         public static void countArrayDuplicates() {
             ArrayList<Integer> numbers = new ArrayList();
             numbers.add(0,1);
-            numbers.add(1,1);
+            numbers.add(1,10);
             numbers.add(2,1);
             numbers.add(3,2);
             numbers.add(4,2);
+            numbers.add(5,3);
+            numbers.add(6,7);
+            numbers.add(7,1);
+            numbers.add(8,10);
 
 
             Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
-
-            for (int i = 0; i < numbers.size(); i++) {
-                int key = numbers.get(i);
-                if (countMap.containsKey(key)) {
-                    int count = countMap.get(key);
-                    count++;
-                    countMap.put(key, count); // Hashes can't have two duplicate keys with different values
-                } else {                    //with different values
-                    countMap.put(key, 1); //so key is overwritten until final value is obtained
-                }
+            for (int i : numbers) {  //for every number in array
+                Integer j = countMap.get(i); // that line is void. resets counter
+                countMap.put(i, (j == null) ? 1 : j + 1); //ternary
             }
-            System.out.print("Duplicate results:");
-            System.out.println(countMap);
+
+            System.out.println("Duplicates: " + countMap);
         }
     }
 
